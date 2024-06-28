45abd5c28ec119022df4844871d0455c581d4a77
PeterWang-dev <PeterWang030908@hotmail.com>
2024-06-29 01:36:22 +0800

    refactor(ir_gen): move ir_gen to submodule gen and seperate scope

--- src/gen/ir.rs
+++ src/gen/ir.rs
@@ -1,24 +1,20 @@
+use super::scope::Scope;
 use crate::{ast::*, error::Error};
 use koopa::ir::{self, builder_traits::*, Program, Type};
 
-enum Scope {
-    Program,
-    Function(ir::Function),
-    BasicBlock(ir::Function, ir::BasicBlock),
-}
-
-trait IrGenerator {
+trait GenerateIr {
+    // implement this trait for all types in the AST
     fn generate(&self, program: &mut Program, scope: Scope) -> Result<(), Error>;
 }
 
-impl IrGenerator for CompUnit {
+impl GenerateIr for CompUnit {
     fn generate(&self, program: &mut Program, scope: Scope) -> Result<(), Error> {
         self.func_def.generate(program, scope)?;
         Ok(())
     }
 }
 
-impl IrGenerator for FuncDef {
+impl GenerateIr for FuncDef {
     fn generate(&self, program: &mut Program, scope: Scope) -> Result<(), Error> {
         match scope {
             Scope::Program => (),
@@ -33,18 +29,18 @@ impl IrGenerator for FuncDef {
             },
         );
         let func = program.new_func(func_info);
-        self.block.generate(program, Scope::Function(func))?;
+        self.block.generate(program, Scope::Function(&func))?;
         Ok(())
     }
 }
 
-impl IrGenerator for Block {
+impl GenerateIr for Block {
     fn generate(&self, program: &mut Program, scope: Scope) -> Result<(), Error> {
         let func = match scope {
             Scope::Function(f) => f,
             _ => unreachable!("Block must be in a function scope!"),
         };
-        let func_data = program.func_mut(func);
+        let func_data = program.func_mut(*func);
 
         let entry = func_data
             .dfg_mut()
@@ -54,19 +50,19 @@ impl IrGenerator for Block {
         func_data.layout_mut().bbs_mut().extend([entry]);
 
         self.stmt
-            .generate(program, Scope::BasicBlock(func, entry))?;
+            .generate(program, Scope::BasicBlock(func, &entry))?;
 
         Ok(())
     }
 }
 
-impl IrGenerator for Stmt {
+impl GenerateIr for Stmt {
     fn generate(&self, program: &mut Program, scope: Scope) -> Result<(), Error> {
         let (function, block) = match scope {
             Scope::BasicBlock(t, b) => (t, b),
             _ => unreachable!("Statement must be in a basic block scope!"),
         };
-        let func_data = program.func_mut(function);
+        let func_data = program.func_mut(*function);
 
         let dfg = func_data.dfg_mut();
         let res = dfg.new_value().integer(self.num);
@@ -74,7 +70,7 @@ impl IrGenerator for Stmt {
 
         func_data
             .layout_mut()
-            .bb_mut(block)
+            .bb_mut(*block)
             .insts_mut()
             .extend([ret]);
 
@@ -82,7 +78,7 @@ impl IrGenerator for Stmt {
     }
 }
 
-pub fn generate_ir(ast: &CompUnit) -> Result<Program, Error> {
+pub fn generate_on(ast: &CompUnit) -> Result<Program, Error> {
     let mut program = Program::new();
     ast.generate(&mut program, Scope::Program)?;
     Ok(program)
@@ -105,7 +101,7 @@ mod tests {
             },
         };
 
-        let program = generate_ir(&ast).unwrap();
+        let program = generate_on(&ast).unwrap();
 
         let mut gen = KoopaGenerator::new(Vec::new());
         gen.generate_on(&program).unwrap();
